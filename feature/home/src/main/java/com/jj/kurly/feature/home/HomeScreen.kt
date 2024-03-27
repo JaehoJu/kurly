6d9a50d431c7bf6bdf3e01f90fa96f690b644c97
JAEHOJU <wnwogh88@gmail.com>
2024-03-27 11:45:56 +0900

    vertical section 에서 사용하는 list item 추가

--- feature/home/src/main/java/com/jj/kurly/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/jj/kurly/feature/home/HomeScreen.kt
@@ -1,22 +1,36 @@
 package com.jj.kurly.feature.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -96,7 +110,77 @@ private fun VerticalSectionList(
     products: List<Product>,
     modifier: Modifier = Modifier
 ) {
-    Text(text = "vertical", modifier = modifier)
+    Column(modifier) {
+        products.forEach { VerticalProductListItem(it) }
+    }
+}
+
+@Composable
+private fun VerticalProductListItem(
+    product: Product,
+    modifier: Modifier = Modifier
+) {
+    ListItem(
+        overlineContent = {
+            Box {
+                Text(
+                    text = product.image,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1.5F) // 6 : 4
+                        .background(Color.LightGray)
+                )
+                Text(
+                    text = "wish",
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .background(Color.Yellow)
+                )
+            }
+        },
+        headlineContent = {
+            Text(
+                text = product.name,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        supportingContent = {
+            Row {
+                if (product.discountRate < 100) {
+                    Text(
+                        text = "${product.discountRate}%",
+                        color = colorResource(id = R.color.discount_rate),
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.alignByBaseline()
+                    )
+                    Spacer(modifier = Modifier.width(2.dp))
+                    Text(
+                        text = stringResource(R.string.price, product.discountedPrice),
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.alignByBaseline()
+                    )
+                    Spacer(modifier = Modifier.width(2.dp))
+                    Text(
+                        text = stringResource(R.string.price, product.originalPrice),
+                        style = MaterialTheme.typography.bodySmall
+                            .copy(textDecoration = TextDecoration.LineThrough),
+                        modifier = Modifier.alignByBaseline()
+                    )
+                } else {
+                    Text(
+                        text = stringResource(R.string.price, product.originalPrice),
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        },
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -195,4 +279,49 @@ private fun GridSectionListPreview() {
         isSoldOut = false
     )
     GridSectionList(listOf(product, product, product))
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun VerticalProductListItemPreview() {
+    VerticalProductListItem(
+        product = Product(
+            id = 1,
+            name = "name",
+            image = "image",
+            originalPrice = 1000,
+            discountedPrice = 1000,
+            isSoldOut = false
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun VerticalProductListItemLongNamePreview() {
+    VerticalProductListItem(
+        product = Product(
+            id = 1,
+            name = "name name name name name name name name name name name name",
+            image = "image",
+            originalPrice = 1000,
+            discountedPrice = 1000,
+            isSoldOut = false
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun VerticalProductListItemDiscountedPreview() {
+    VerticalProductListItem(
+        product = Product(
+            id = 1,
+            name = "name",
+            image = "image",
+            originalPrice = 1000,
+            discountedPrice = 500,
+            isSoldOut = false
+        )
+    )
 }
